25ca76c60be74f80dc2b28f446dc9c51c9cf88aa
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-12 22:38:15 -0300

    feat: add selects with country

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall, Pais } from '../../interfaces/paises.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -9,11 +12,58 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SelectorPageComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required],
   });
 
-  constructor(private fb: FormBuilder) {}
+  // llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  fronteras: PaisSmall[] = [];
 
-  ngOnInit(): void {}
+  //UI
+  cargando: boolean = false;
+
+  constructor(private fb: FormBuilder, private paisesService: PaisesService) {}
+
+  ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
+
+    //Cuando cambia la region
+    this.miFormulario
+      .get('region')
+      ?.valueChanges.pipe(
+        tap((_) => {
+          this.miFormulario.get('pais')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap((region) => {
+          return this.paisesService.getPaisesPorRegion(region);
+        })
+      )
+      .subscribe((paises) => {
+        this.paises = paises;
+        this.cargando = false;
+      });
+
+    //Cuando cambia el pais
+    this.miFormulario
+      .get('pais')
+      ?.valueChanges.pipe(
+        tap((_) => {
+          this.miFormulario.get('frontera')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap((codigo) => this.paisesService.getPaisesPorCodigo(codigo)),
+        switchMap((pais) =>
+          this.paisesService.getPaisesPorCodigos(pais?.borders)
+        )
+      )
+      .subscribe((paises) => {
+        this.fronteras = paises;
+        this.cargando = false;
+      });
+  }
 
   guardar() {
     console.log(this.miFormulario.value);
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PaisSmall, Pais } from '../interfaces/paises.interface';
+import { combineLatest, Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisesService {
+  private baseUrl = 'https://restcountries.com/v3.1';
+  private _regiones: string[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
 
-  constructor() { }
+  get regiones(): string[] {
+    return [...this._regiones];
+  }
+
+  constructor(private http: HttpClient) {}
+
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]> {
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name`;
+    return this.http.get<PaisSmall[]>(url);
+  }
+
+  getPaisesPorCodigo(codigo: string): Observable<Pais[] | null> {
+    if (!codigo) return of(null);
+    const url: string = `${this.baseUrl}/alpha/${codigo}`;
+    return this.http.get<Pais[]>(url);
+  }
+
+  getPaisesPorCodigoSmall(codigo: string): Observable<PaisSmall> {
+    const url: string = `${this.baseUrl}/alpha/${codigo}?fields=cca3,name`;
+    return this.http.get<PaisSmall>(url);
+  }
+
+  getPaisesPorCodigos(borders: string[]): Observable<PaisSmall[]> {
+    if (!borders) return of([]);
+
+    const peticiones: Observable<PaisSmall>[] = [];
+
+    borders.forEach((codigo) => {
+      const peticion = this.getPaisesPorCodigoSmall(codigo);
+      peticiones.push(peticion);
+    });
+
+    return combineLatest(peticiones);
+  }
 }
